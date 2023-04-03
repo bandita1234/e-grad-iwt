f46a82b59e64208bb516cee372f15bc4ba1b7ce7
Partha Sarathi Praharaj <79869032+partha6686@users.noreply.github.com>
2023-04-03 09:18:23 +0000

    Result bug fixed

--- src/components/ExamResult.js
+++ src/components/ExamResult.js
@@ -5,25 +5,26 @@ const ExamResult = () => {
   const exam = useContext(ExamContext);
   const [ques, setQues] = useState();
   const calcRes = (tmp) => {
-    // console.log(tmp[0].questions,exam.res);
     let sc = 0;
-    exam && exam.res.map((idx,element) => {
-      if(tmp[0].questions[idx].choices[tmp[0].questions[idx].correctAnswer] === element ){
+    for (let idx = 0; idx < tmp[0].totalQuestions; idx++) {
+      // const element = array[idx];
+      if(tmp[0].questions[idx].choices[tmp[0].questions[idx].correctAnswer] ==exam.res[idx] ){
         sc+=1;
       }
-    });
+    }
     exam.setScore(sc);
     console.log(sc);
   }
   useEffect(() => {
     const tmp = quiz.filter((q) => q.topic===exam.subject);
+    console.log("1",tmp[0]);
     setQues([...tmp]);
     calcRes(tmp);
   }, []);
   return (
     <div className="bg-emerald-100 h-screen lg:py-10 lg:px-20 flex justify-center items-center">
     <div className="bg-emerald-200 w-full sm:w-1/2 p-8 space-y-4 rounded-md">
-    {exam && <h1>Your Total Score is : {exam.score}</h1>}
+    {exam &&exam.score && <h1>Your Total Score is : {exam.score}</h1>}
     <p></p>
     </div>
     </div>
