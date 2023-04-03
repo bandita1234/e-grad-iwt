5dd3644d3953609e326e141fb6cb9b858916e8c7
Partha Sarathi Praharaj <79869032+partha6686@users.noreply.github.com>
2023-04-03 09:41:43 +0000

    Adder redirect btn

--- src/components/ExamResult.js
+++ src/components/ExamResult.js
@@ -7,26 +7,28 @@ const ExamResult = () => {
   const calcRes = (tmp) => {
     let sc = 0;
     for (let idx = 0; idx < tmp[0].totalQuestions; idx++) {
-      // const element = array[idx];
-      if(tmp[0].questions[idx].choices[tmp[0].questions[idx].correctAnswer] ==exam.res[idx] ){
-        sc+=1;
+      if (tmp[0].questions[idx].choices[tmp[0].questions[idx].correctAnswer] == exam.res[idx]) {
+        sc += 1;
       }
     }
     exam.setScore(sc);
     console.log(sc);
   }
   useEffect(() => {
-    const tmp = quiz.filter((q) => q.topic===exam.subject);
-    console.log("1",tmp[0]);
+    const tmp = quiz.filter((q) => q.topic === exam.subject);
     setQues([...tmp]);
     calcRes(tmp);
   }, []);
   return (
     <div className="bg-emerald-100 h-screen lg:py-10 lg:px-20 flex justify-center items-center">
-    <div className="bg-emerald-200 w-full sm:w-1/2 p-8 space-y-4 rounded-md">
-    {exam &&exam.score && <h1>Your Total Score is : {exam.score}</h1>}
-    <p></p>
-    </div>
+      <div className="bg-emerald-200 w-full sm:w-1/2 p-8 space-y-4 rounded-md">
+        <p>
+          {exam && exam.score && <h1>Your Total Score is : {exam.score}</h1>}
+        </p>
+        <button className="bg-emerald-400 my-8 py-2 px-4 rounded-md text-white font-semibold">
+          <a href="/" >Home</a>
+        </button>
+      </div>
     </div>
   )
 }
